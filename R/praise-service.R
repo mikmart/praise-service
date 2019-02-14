68c2aa7e2993dd1a7db3711876859868a80671f9
Mikko Marttila <mikmart@mikkomarttila.com>
2019-02-14 21:14:25 +0200

    Add /praise/slack

--- R/praise-service.R
+++ R/praise-service.R
@@ -7,3 +7,13 @@ praise::praise
 function() {
   praise::praise_parts
 }
+
+#* An endpoint to serve Slack post requests
+#* @param text the user-typed message
+#* @post /praise/slack
+function(text = "You are ${adjective}!") {
+  list(
+    response_type = "in_channel",
+    text = praise::praise(text)
+  )
+}
